1d6522b4b3430aff08fe7a69ab22ff3858affa77
John Moran <johnnymoran44@hotmail.com>
2019-09-12 22:15:44 +0200

    good

--- src/components/About.js
+++ src/components/About.js
@@ -7,6 +7,10 @@ const About = () => {
         <h2>About John Moran</h2>
         <p>Tengo muchas ganas trabajar en Front End y usar React, Vue o Angular. Tengo un pasíon aprender y hacer más proyectos</p>
         <h3>Sobre esta prueba:</h3>
+        <a href="https://github.com/artworkjpm/bestiario-dropDownChart" target="_blank" rel="noopener noreferrer">
+          <img src="github-brands.svg" alt="" height="30" />
+          <span className="centerLink">Link to Github repo</span>
+        </a>
         <p>
           He decidido usar <b>React</b> porque últimamente estoy usando React la mayoría de tempo,( y me encanta CREATE-REACT-APP! programa), pero he hecho proyectos en <b>Vue.js</b> y <b>Angular </b>y no me importa cual lo uso para proyectos. Para esto prueba era bastante fácil porque recientemente he hecho un proyecto usando <b>Chart.js</b>, y tengo conocimientos a usarlo.
           Prefiero usar <b>Chart.js</b> en lugar de <b>D3.js</b> porque he encontrado <b>chart.js</b> estar más fácil, pero también he hecho un proyecto con <b>d3.js</b> antes. Solo quiero juntar un equipo y hacer cosas de
